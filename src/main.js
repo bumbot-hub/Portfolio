6747ff3bc638dd0d4e626b90b9c825d19b7adc07
Kamil Kula <xkamil580@gmail.com>
2026-09-13 01:36:09 +0200

    fix: change direction of png files

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ document.querySelector('#app').innerHTML = `
         <h4>Product Designer<br>& Developer</h4>
     </section>
     <div class="dot-reveal"></div>
-    <img src='src/assets/ChatGPT.png' alt='człowiek' class="hero-figure"> 
+    <img src='/images/ChatGPT.png' alt='człowiek' class="hero-figure"> 
 </section>
 
 <section id="about_me">
